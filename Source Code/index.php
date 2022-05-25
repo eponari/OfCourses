8f39e3ddd0a6b2a28052b0d3aed788a7d8c3c44d
Edison Ponari <eponari19@epoka.edu.al>
2022-05-25 21:37:41 +0200

    Added index and login page

---
+++ Source Code/index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Welcome to OfCourses.</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Template Main CSS File -->
+  <link href="css/indexStyle.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  
+      <div class="container">
+        <nav class="navbar">
+        <div class="container-fluid">
+          
+        
+            <a class="button" href="indexPage.html">OfCourses</a>
+           
+            <a class="btn" href="contact.html">Contact Us</a>
+            <a class="btn" href="about.html">About</a>
+            <a class="btn" href="signup.php">Sign Up</a></li>
+            <a class="btn" href="login.php">Log In</a></li>
+        </div>
+       
+      </div>
+    </nav>
+  </header><!-- End Header -->
+
+ 
+    <img src="images/image.jpg" class="container-one">
+    <div class="container d-flex flex-column justify-content-center align-items-center text-center position-relative" data-aos="zoom-out">
+     
+      <h2>Welcome to <span>OfCourses!</span></h2>
+      <p>"To know, is to know that you know nothing. That is the meaning of true knowledge." - Socrates.</p>
+      
+  </section>
+
+  <main id="main">
+
+       <h4> Explore trending courses below.
+        <br> <a class="buttonExtra" href="categories.html">Browse through categories.</a>
+       </h4>
+      
+    
+    <div class="flex-container">
+
+      <div class="border">
+        
+        <h4><a href="" class="stretched-link">Code With Us!</a></h4>
+       <p> Introduction to programming languages.
+         <br> This course teaches the fundamentals
+         <br> of the most common languages. </p>
+       </p>
+       <a class="btnExtra" href="registerCourse.html">Register</a>
+       <h5> Beginner. 5-6 months. </h5>
+       
+      </div>
+      <div class="border">
+        <h4><a href="" class="stretched-link">Web Design Development.</a></h4>
+        <p>Learn how to use HTML, CSS, Javascript
+          <br> and PHP to build a website
+          <br> from scratch. 
+        </p>
+        <a class="btnExtra" href="registerCourse.html">Register</a>
+        <h5> Beginner. 3-4 months. </h5>
+      </div>
+      <div class="border">
+        <h4><a href="" class="stretched-link">Get started with Data Analysis.</a></h4>
+        <p>Dive into the implementation of
+          <br> Python, Java and SQL in the
+          <br> major data analysis methods.</p>
+          <a class="btnExtra" href="registerCourse.html">Register</a>
+          <h5> Beginner. 1-3 months. </h5>
+      </div>
+      <div class="border">
+        <h4><a href="" class="stretched-link">Digital Media and Mobile Applications.</a></h4>
+        <p>Learn about the different alternatives
+          <br> of development approaches when
+          <br> building mobile applications. </p>
+          <a class="btnExtra" href="registerCourse.html">Register</a>
+          <h5> Intermediate. 6 months. </h5>
+        </p>
+      </div>
+      <div class="border">
+        <h4><a href="" class="stretched-link">Introduction basics to Cyber Security.</a></h4>
+        <p>Common types of cyber security 
+          <br> threats and how you can learn to
+          <br> work with solving them. </p>
+          <a class="btnExtra" href="registerCourse.html">Register</a>
+          <h5> Beginner. 3-4 months. </h5>
+        </p>
+      </div>
+    </div>
+
+
+
+
+  </body>
