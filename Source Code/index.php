2be6047d08454bc33bce4832337b73e6b687390a
klea <102556763+kleakota@users.noreply.github.com>
2022-06-02 19:02:43 +0200

    Update index.php

--- Source Code/index.php
+++ Source Code/index.php
@@ -27,8 +27,8 @@
         
             <a class="button" href="indexPage.html">OfCourses</a>
            
-            <a class="btn" href="contact.html">Contact Us</a>
-            <a class="btn" href="about.html">About</a>
+            <a class="btn" href="indexPage.html#contactUs">Contact Us</a>
+            <a class="btn" href="indexPage.html#about">About</a>
             <a class="btn" href="signup.php">Sign Up</a></li>
             <a class="btn" href="login.php">Log In</a></li>
         </div>
@@ -103,7 +103,73 @@
       </div>
     </div>
 
+   <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-header">
+          <h2>About Us.</h2>
+          <h3 class="pt-0 pt-lg-5" style=" font-size: 15px; text-decoration: underline; text-align: center;">Get to know our team members better!</h3>
+
+          <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+
+            <div class="col-lg-5">
+              <div class="about-img">
+                <img src="images/image4.jpg" class="img-fluid" alt="">
+              </div>
+            </div>
+  
+            <div class="col-lg-7">
+          
+
+       <fieldset>
+         <h4 style="text-decoration: underline; padding-bottom: 30px;"> Who we are</h4>
+         <p style="text-justify:inter-word; color: black;"> Launched in 2022 as one of the leading applications and services
+           <br> providing the newest and latest features to our users and 
+           <br> bringing forwards an improved and entertaining way of
+           <br> pursuing courses of every possible field of study.
+         </p>
+       </fieldset><br><br>
+  
+
+       <fieldset>
+        <h4 style="text-decoration: underline;"> Our commitment to you</h4>
+        <p style="text-justify:inter-word; color: black;"> We ensure that this experience will
+          <br> leave you satisfied as we guarantee:
+          <br><li style="text-align:center; color:black;"> Work Ethic.</li> 
+          <br><li style="text-align:center; color: black;">Transparency.</li>
+          <br><li style="text-align:center; color: black;">Resourcefulness.</li>
+        </p>
+      </fieldset>
 
 
+      <section id="contactUs" class="contactUs">
+        <div class="container" data-aos="fade-up">
+  
+          <div class="section-header">
+            <h2>Contact Us.</h2>
+            <h3 class="pt-0 pt-lg-5" style=" font-size: 15px; text-decoration: underline; text-align: center;">You can find all of our contact information below.</h3>
+  
+            <div class="row g-4 g-lg-5" data-aos="fade-up" data-aos-delay="200">
+  
+              <div class="col-lg-5">
+                <div class="about-img">
+                  <img src="images/image5.jpg" style="padding-bottom:40px;" class="img-fluid" alt="">
+                </div>
+              </div>
+    
+              <div class="col-lg-7">
+            
+  
+         <fieldset >
+           <h4 style="text-decoration: underline; padding-bottom: 5px;"> Talk to us on multiple platforms.</h4>
+
+           <div class="img-container">
+           <p style="text-justify:inter-word; color: black; font-size: 12px; margin-right:43px;  font-weight: bold;"> <img src="images/instagram.png" style="width:5%;"> @OfCourses </p>
+            <p style="text-justify:inter-word; color: black; font-size: 12px; font-weight: bold;"> <img src="images/twitter.png" style="width:5%;"> @OfCourses_official </p>
+            <p style="text-justify:inter-word; color: black; font-size: 12px;  margin-right:5px;  font-weight: bold;"> <img src="images/facebook.png" style="width:5%;"> @OfCourses_Page </p>
+          
+          </div>
+         </fieldset><br><br>
+  
 
   </body>
